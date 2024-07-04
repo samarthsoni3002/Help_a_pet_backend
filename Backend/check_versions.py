b49174c07d4ac40e43515d0c10711abdd136c7c2
samarthsoni3002 <samarthsoni3002@gmail.com>
2024-07-04 11:47:25 +0530

    versions added

---
+++ Backend/check_versions.py
@@ -0,0 +1,13 @@
+import flask
+import tensorflow as tf
+import PIL
+import numpy as np
+import flask_cors
+import pip
+
+print("Flask:", flask.__version__)
+print("TensorFlow:", tf.__version__)
+print("Pillow:", PIL.__version__)
+print("NumPy:", np.__version__)
+print("Flask-Cors:", flask_cors.__version__)
+print("pip:", pip.__version__)
